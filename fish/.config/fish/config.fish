de36287a7bc46fd4cc65cdc9600fa5fd2eb15ba2
Tim Carlsson <timcarlsson0203@gmail.com>
2025-07-01 14:30:27 +0200

    tmux

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,16 @@ set -U fish_user_paths /opt/nvim $fish_user_path
 # Aliases
 alias neofetch='fastfetch'
 
+
+# abbreviations
+abbr --add gs 'git status'
+abbr --add gc 'git checkout'
+abbr --add ga 'git add .'
+abbr --add gp 'git push'
+
+abbr --add fishs 'source ~/.config/fish/config.fish'
+abbr --add tmuxs 'tmux source ~/.config/tmux/tmux.conf'
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
